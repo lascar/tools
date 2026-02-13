042600c00634d708c1d7a93c4647390f408ea6b5
pascal.carrie@gmail.com <pascal.carrie@gmail.com>
2026-02-13 11:59:38 +0100

    130226-portatil

--- commit.sh
+++ commit.sh
@@ -3,17 +3,22 @@
 branch=$(git rev-parse --abbrev-ref HEAD)
 echo "branch : $branch"
 read -r typ num reste <<< $(echo $branch|perl -ane '/(?:#)?(feature|tickets)\/(?:#)?(?:t_)?([0-9]+)(_.*)/ and print"$1 $2 $3"')
-if [[ $typ == "tickets" ]]
+if [[ $branch != "master" ]]
 then
-  typo="t_"
-fi
-files_to_commit=$(git status|perl -ane '(/modified|renamed|deleted/ and print "$F[1] ") or (/new file/ and print "$F[2] ")')
-echo "files_to_commit : $files_to_commit"
-message="$typo$num$reste: $1"
-echo "message : $message"
-if [[ $2 == 'nv' ]]
-then
-  git commit --no-verify $(echo $files_to_commit) -m "$message"
-else
-  git commit  $(echo $files_to_commit) -m "$message"
+  if [[ $typ == "tickets" ]]
+  then
+    typo="t_"
+  fi
+  files_to_commit=$(git status|perl -ane '(/modified|renamed|deleted/ and print "$F[1] ") or (/new file/ and print "$F[2] ")')
+  echo "files_to_commit : $files_to_commit"
+  message="$typo$num$reste: $1"
+  echo "message : $message"
+  if [[ $2 == 'nv' ]]
+  then
+    git commit --no-verify $(echo $files_to_commit) -m "$message"
+  else
+    git commit  $(echo $files_to_commit) -m "$message"
+  fi
+  else
+  echo "pas de commit sur master"
 fi
--- recurs_grep.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if test -z "$2"
-then
-  dir="app/"
-else
-  dir=$2
-fi
-rgrep "$1" $dir 2>/dev/null |egrep -v '~:|^Bi|^grep'
